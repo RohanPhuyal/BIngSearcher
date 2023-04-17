92ffd6a508d19499d1fbb639a77d443bead4e620
RoNzzzz <rohanphuyal2@gmail.com>
2023-04-17 18:52:18 +0545

    Stop Working

--- background.js
+++ background.js
@@ -1,104 +1,100 @@
+var intervalId;
 function performSearches(numSearches, searchType) {
-    var searchUrl;
-    if (searchType === "desktop") {
-      searchUrl = "https://www.bing.com/search?q=";
-    } else if (searchType === "mobile") {
-      searchUrl =
-        "https://www.bing.com/search?q=&qs=n&form=QBRE&sp=-1&pq=&sc=8-0&sk=&cvid=&first=1";
-    } else {
-      console.error("Invalid search type: " + searchType);
+  var searchUrl;
+  if (searchType === "desktop") {
+    searchUrl = "https://www.bing.com/search?q=";
+  } else if (searchType === "mobile") {
+    searchUrl =
+      "https://www.bing.com/search?q=&qs=n&form=QBRE&sp=-1&pq=&sc=8-0&sk=&cvid=&first=1";
+  } else {
+    console.error("Invalid search type: " + searchType);
+    return;
+  }
+
+  var searchCount = 0;
+  var prevSearches = [];
+  intervalId = setInterval(function () {
+    if (searchCount >= numSearches) {
+      clearInterval(intervalId);
+      console.log("Finished performing searches.");
       return;
     }
-  
-    var searchCount = 0;
-    var prevSearches = [];
-    var intervalId = setInterval(function () {
-      if (searchCount >= numSearches) {
-        clearInterval(intervalId);
-        console.log("Finished performing searches.");
-        return;
-      }
-  
-      var searchTerm = generateSearchTerm();
-      if (prevSearches.includes(searchTerm)) {
-        console.log("Skipping duplicate search term: " + searchTerm);
-        return;
-      }
-      prevSearches.push(searchTerm);
-  
-      var url = searchUrl + encodeURIComponent(searchTerm);
-      chrome.tabs.update({ url: url }, function (tab) {
-        console.log("Searching for: " + searchTerm);
-      });
-  
-      searchCount++;
-    }, 1000);
-  }
-  
-  chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
-    if (message.type === "start-searches") {
-      performSearches(message.numSearches, message.searchType);
+
+    var searchTerm = generateSearchTerm();
+    if (prevSearches.includes(searchTerm)) {
+      console.log("Skipping duplicate search term: " + searchTerm);
+      return;
     }
-  });
-  
-  
-  function generateSearchTerm() {
-    var adjectives = [
-      "famous",
-      "delicious",
-      "scary",
-      "fantastic",
-      "beautiful",
-      "silly",
-      "colorful",
-      "exciting",
-      "amazing",
-      "weird",
-      "happy",
-      "embarrassing",
-      "tall",
-      "delicious",
-      "uncomfortable",
-      "clumsy",
-      "suspicious",
-      "goofy",
-      "cowardly",
-      "brave",
-      "groovy",
-      
-    ];
-    var nouns = [
-      "dog",
-      "cat",
-      "robot",
-      "unicorn",
-      "dragon",
-      "spaceship",
-      "planet",
-      "piano",
-      "jungle",
-      "mountain",
-      "ron",
-      "sagar",
-      "bipin",
-      "lee",
-      "jadu",
-      "game",
-      "valo",
-      "rohan",
-      "rito",
-      "video",
-    ];
-  
-    var adjIndex = Math.floor(Math.random() * adjectives.length);
-    var nounIndex = Math.floor(Math.random() * nouns.length);
-  
-    return adjectives[adjIndex] + " " + nouns[nounIndex];
+    prevSearches.push(searchTerm);
+
+    var url = searchUrl + encodeURIComponent(searchTerm);
+    chrome.tabs.update({ url: url }, function (tab) {
+      console.log("Searching for: " + searchTerm);
+    });
+
+    searchCount++;
+  }, 1000);
+}
+
+chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
+  if (message.type === "start-searches") {
+    performSearches(message.numSearches, message.searchType);
+  } else if (message.type === "stop-searches") {
+    clearInterval(intervalId);
+    console.log("Stopped performing searches.");
   }
-  
-  chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
-    if (message.type === "start-searches") {
-      performSearches(message.numSearches, message.searchType);
-    }
-  });
-  
+});
+
+function generateSearchTerm() {
+  var adjectives = [
+    "famous",
+    "delicious",
+    "scary",
+    "fantastic",
+    "beautiful",
+    "silly",
+    "colorful",
+    "exciting",
+    "amazing",
+    "weird",
+    "happy",
+    "embarrassing",
+    "tall",
+    "delicious",
+    "uncomfortable",
+    "clumsy",
+    "suspicious",
+    "goofy",
+    "cowardly",
+    "brave",
+    "groovy",
+    
+  ];
+  var nouns = [
+    "dog",
+    "cat",
+    "robot",
+    "unicorn",
+    "dragon",
+    "spaceship",
+    "planet",
+    "piano",
+    "jungle",
+    "mountain",
+    "ron",
+    "sagar",
+    "bipin",
+    "lee",
+    "jadu",
+    "game",
+    "valo",
+    "rohan",
+    "rito",
+    "video",
+  ];
+
+  var adjIndex = Math.floor(Math.random() * adjectives.length);
+  var nounIndex = Math.floor(Math.random() * nouns.length);
+
+  return adjectives[adjIndex] + " " + nouns[nounIndex];
+}
--- popup.js
+++ popup.js
@@ -1,16 +1,26 @@
 document.addEventListener("DOMContentLoaded", function () {
-    var form = document.getElementById("search-form");
-  
-    form.addEventListener("submit", function (event) {
-      event.preventDefault();
-      var numSearches = document.getElementById("num-searches").value;
-      var searchType = document.getElementById("search-type").value;
-  
-      chrome.runtime.sendMessage({
-        type: "start-searches",
-        numSearches: numSearches,
-        searchType: searchType,
-      });
+  var form = document.getElementById("search-form");
+  var stopButton = document.getElementById("stop-button");
+
+  var intervalId;
+
+  form.addEventListener("submit", function (event) {
+    event.preventDefault();
+    var numSearches = document.getElementById("num-searches").value;
+    var searchType = document.getElementById("search-type").value;
+
+    chrome.runtime.sendMessage({
+      type: "start-searches",
+      numSearches: numSearches,
+      searchType: searchType,
     });
+
+    // Store the interval ID so that it can be cleared later
+    intervalId = setInterval(function () {}, 1000);
   });
-  
+
+  stopButton.addEventListener("click", function (event) {
+    clearInterval(intervalId);
+    chrome.runtime.sendMessage({ type: "stop-searches" });
+  });
+});
