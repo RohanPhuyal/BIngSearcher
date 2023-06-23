078540a32baadadaf039e276c8c269387ed04860
RoNzzzz <rohanphuyal2@gmail.com>
2023-06-23 19:07:21 +0545

    v7.5

--- gamefix.js
+++ gamefix.js
@@ -1,31 +1,31 @@
-(function () {
+(async function () {
     var iterationNumber = 0;
     var selectButton = null;
     var selectExists = null;
-    var fixIntervalId;
-    var scroll= document.querySelector("shopping-page-base")
+    var scroll = document.querySelector("shopping-page-base")
       ?.shadowRoot.querySelector("shopping-homepage")
       ?.shadowRoot.querySelector("msft-feed-layout")
       ?.shadowRoot.querySelector("msn-shopping-game-pane");
-
-      if(scroll){
-        scroll.scrollIntoView({behavior: 'smooth'});
-      }else{
-        alert("Couldn't Load Game, Reload");
-		return;
-      }
+  
+    if (scroll) {
+      scroll.scrollIntoView({ behavior: 'smooth' });
+    } else {
+      alert("Couldn't Load Game, Reload");
+      return;
+    }
+  
     var msnShoppingGamePane2 = document.querySelector("shopping-page-base")
-        ?.shadowRoot.querySelector("shopping-homepage")
-        ?.shadowRoot.querySelector("msft-feed-layout")
-        ?.shadowRoot.querySelector("msn-shopping-game-pane");
-
+      ?.shadowRoot.querySelector("shopping-homepage")
+      ?.shadowRoot.querySelector("msft-feed-layout")
+      ?.shadowRoot.querySelector("msn-shopping-game-pane");
+  
     var shoppingGame = document.querySelector("shopping-page-base")
-        ?.shadowRoot.querySelector("shopping-homepage")
-        ?.shadowRoot.querySelector("msft-feed-layout")
-        ?.shadowRoot.querySelector("msn-shopping-game-pane")
-        ?.shadowRoot.querySelector("msft-stripe");
-
-    function getObjectIndexFromArray(objects, id) {
+      ?.shadowRoot.querySelector("shopping-homepage")
+      ?.shadowRoot.querySelector("msft-feed-layout")
+      ?.shadowRoot.querySelector("msn-shopping-game-pane")
+      ?.shadowRoot.querySelector("msft-stripe");
+  
+      async function getObjectIndexFromArray(objects, id) {
         for (let i = 0; i < objects.length; i++) {
             if (objects[i].id === id) {
                 return i;
@@ -33,7 +33,8 @@
         }
         return -1;
     }
-    function getLowestPriceItemID(priceMap) {
+    
+    async function getLowestPriceItemID(priceMap) {
         var lowestPriceItemID;
         var currLowestPrice = Infinity;
         for (const [itemID, item] of Object.entries(priceMap)) {
@@ -44,8 +45,8 @@
         }
         return lowestPriceItemID;
     }
-
-    function highlightItems(correctIndex, items) {
+    
+    async function highlightItems(correctIndex, items) {
         for (let i = 0; i < items.length; i++) {
             if (i == correctIndex) {
                 selectButton = shoppingGame.getElementsByClassName("shopping-game-card-outline")[correctIndex]
@@ -56,61 +57,63 @@
             }
         }
     }
-    function refreshGame() {
-        var lowestPriceItemID = getLowestPriceItemID(msnShoppingGamePane2.originalPricesbyId);
-        var itemIndex = getObjectIndexFromArray(msnShoppingGamePane2.displayedShoppingEntities, lowestPriceItemID);
-        console.log("Coeeect Index: "+itemIndex);
-        highlightItems(itemIndex, shoppingGame.getElementsByClassName("shopping-game-card-outline"));
-        if (selectButton != null) {
-                    console.log("SEL Button received" + selectButton);
-                    setTimeout(selectButtonCLick, 1000);
-                } else {
-                    console.log("Select Button Null");
-                }
-        const msnShoppingGamePane = document.querySelector("shopping-page-base")
+    
+  
+    async function refreshGame() {
+        var lowestPriceItemID = await getLowestPriceItemID(msnShoppingGamePane2.originalPricesbyId);
+        var itemIndex = await getObjectIndexFromArray(msnShoppingGamePane2.displayedShoppingEntities, lowestPriceItemID);
+        console.log("Correct Index: " + itemIndex);
+        await highlightItems(itemIndex, shoppingGame.getElementsByClassName("shopping-game-card-outline"));
+      if (selectButton != null) {
+        console.log("SEL Button received" + selectButton);
+        await selectButtonCLick();
+      } else {
+        console.log("Select Button Null");
+      }
+      const msnShoppingGamePane = document.querySelector("shopping-page-base")
         ?.shadowRoot.querySelector("shopping-homepage")
         ?.shadowRoot.querySelector("msft-feed-layout")
         ?.shadowRoot.querySelector("msn-shopping-game-pane");
-        if (msnShoppingGamePane.getAttribute('gamestate')=='win'||msnShoppingGamePane.getAttribute('gamestate')=='idle'||msnShoppingGamePane.getAttribute('gamestate')=='lose') {
-            msnShoppingGamePane.setAttribute('gamestate', 'active');
-            msnShoppingGamePane.resetGame();
-        }
+      if (msnShoppingGamePane.getAttribute('gamestate') == 'win' || msnShoppingGamePane.getAttribute('gamestate') == 'idle' || msnShoppingGamePane.getAttribute('gamestate') == 'lose') {
+        msnShoppingGamePane.setAttribute('gamestate', 'active');
+        msnShoppingGamePane.resetGame();
+      }
     }
-    function selectButtonCLick() {
-        console.log("Iteration no: "+iterationNumber)
-        selectButton.click();
-        iterationNumber++;
-        if(iterationNumber>=10){
-            stopFixExecution();
-        }
-    }
-    refreshGame();
-    msnShoppingGamePane2.addEventListener('click', function (event) {
-        console.log("MOUSE");
-        refreshGame();
-    });
-
-    function executeFixFunction() {
-        selectExists = document.querySelector("shopping-page-base")
-            ?.shadowRoot.querySelector("shopping-homepage")
-            ?.shadowRoot.querySelector("msft-feed-layout")
-            ?.shadowRoot.querySelector("msn-shopping-game-pane").getAttribute("gamestate");
-        console.log("Execute Fix Function: " + selectExists);
-        if (selectExists == 'win' || selectExists == 'idle' || selectExists == 'active' || selectExists == 'lose') {
-            refreshGame();
-        }
-    }
-
-    function startFixExecution() {
-        console.log("Start Fix Function");
-        fixIntervalId = setInterval(function () {
-            executeFixFunction();
-        }, 1100);
-    }
-    startFixExecution();
-    function stopFixExecution() {
-        // Clear the interval
+  
+    async function selectButtonCLick() {
+      console.log("Iteration no: " + iterationNumber)
+      selectButton.click();
+      iterationNumber++;
+      if (iterationNumber >= 10) {
         clearInterval(fixIntervalId);
+      }
     }
-
-})();
+  
+    await refreshGame();
+  
+    msnShoppingGamePane2.addEventListener('click', async function (event) {
+      console.log("MOUSE");
+      await refreshGame();
+    });
+  
+    async function executeFixFunction() {
+      selectExists = document.querySelector("shopping-page-base")
+        ?.shadowRoot.querySelector("shopping-homepage")
+        ?.shadowRoot.querySelector("msft-feed-layout")
+        ?.shadowRoot.querySelector("msn-shopping-game-pane").getAttribute("gamestate");
+      console.log("Execute Fix Function: " + selectExists);
+      if (selectExists == 'win' || selectExists == 'idle' || selectExists == 'active' || selectExists == 'lose') {
+        await refreshGame();
+      }
+    }
+  
+    async function startFixExecution() {
+      console.log("Start Fix Function");
+      fixIntervalId = setInterval(async function () {
+        await executeFixFunction();
+      }, 1100);
+    }
+  
+    await startFixExecution();
+  })();
+  
--- gamemanual.js
+++ gamemanual.js
@@ -1,18 +1,18 @@
-(function () {
+(async function () {
     var scroll= document.querySelector("shopping-page-base")
-      ?.shadowRoot.querySelector("shopping-homepage")
-      ?.shadowRoot.querySelector("msft-feed-layout")
-      ?.shadowRoot.querySelector("msn-shopping-game-pane");
+        ?.shadowRoot.querySelector("shopping-homepage")
+        ?.shadowRoot.querySelector("msft-feed-layout")
+        ?.shadowRoot.querySelector("msn-shopping-game-pane");
 
-      if(scroll){
+    if (scroll) {
         scroll.scrollIntoView({behavior: 'smooth'});
-      }else{
+    } else {
         alert("Couldn't Load Game, Reload");
-		return;
-      }
+        return;
+    }
     var selectButton = null;
     var selectExists = null;
-    var iterationNumber=0;
+    var iterationNumber = 0;
     var msnShoppingGamePane2 = document.querySelector("shopping-page-base")
         ?.shadowRoot.querySelector("shopping-homepage")
         ?.shadowRoot.querySelector("msft-feed-layout")
@@ -24,100 +24,111 @@
         ?.shadowRoot.querySelector("msn-shopping-game-pane")
         ?.shadowRoot.querySelector("msft-stripe");
 
-    function getObjectIndexFromArray(objects, id) {
+    async function getObjectIndexFromArray(objects, id) {
         for (let i = 0; i < objects.length; i++) {
             if (objects[i].id === id) {
-                return i
+                return i;
             }
         }
-        return -1
+        return -1;
     }
-    function getLowestPriceItemID(priceMap) {
+
+    async function getLowestPriceItemID(priceMap) {
         var lowestPriceItemID;
         var currLowestPrice = Infinity;
         for (const [itemID, item] of Object.entries(priceMap)) {
             let price = parseFloat(item.price.substring(1));
             if (currLowestPrice > price) {
-                lowestPriceItemID = itemID; currLowestPrice = price
+                lowestPriceItemID = itemID;
+                currLowestPrice = price;
             }
         }
-        return lowestPriceItemID
+        return lowestPriceItemID;
     }
 
-    function highlightItems(correctIndex, items) {
+    async function highlightItems(correctIndex, items) {
         for (let i = 0; i < items.length; i++) {
-            if (i == correctIndex) {
+            if (i === correctIndex) {
                 selectButton = shoppingGame.getElementsByClassName("shopping-game-card-outline")[correctIndex]
                     ?.querySelector("fluent-card")
                     ?.querySelector("msn-shopping-card")
                     .getElementsByClassName("shopping-select-overlay-button")[0];
                 items[i].style.borderColor = "red";
-
+            } else {
+                items[i].style.borderColor = "";
+                items[i].style.display = "none";
             }
-            else {
-                items[i].style.borderColor = ""; items[i].style.display = "none"; 
-               } 
         }
     }
-    function refreshGame() {
-        var lowestPriceItemID = getLowestPriceItemID(msnShoppingGamePane2.originalPricesbyId);
-        var itemIndex = getObjectIndexFromArray(msnShoppingGamePane2.displayedShoppingEntities, lowestPriceItemID);
-        console.log("Coeeect Index: "+itemIndex);
-        highlightItems(itemIndex, shoppingGame.getElementsByClassName("shopping-game-card-outline"));
+
+    async function refreshGame() {
+        var lowestPriceItemID = await getLowestPriceItemID(msnShoppingGamePane2.originalPricesbyId);
+        var itemIndex = await getObjectIndexFromArray(msnShoppingGamePane2.displayedShoppingEntities, lowestPriceItemID);
+        console.log("Coeeect Index: " + itemIndex);
+        await highlightItems(itemIndex, shoppingGame.getElementsByClassName("shopping-game-card-outline"));
         if (selectButton != null) {
-                    console.log("SEL Button received" + selectButton);
-                    setTimeout(selectButtonCLick, 1000);
-                } else {
-                    console.log("Select Button Null");
-                }
-                if (msnShoppingGamePane2.getAttribute('gamestate')=='idle'){
-                    msnShoppingGamePane2.setAttribute('gamestate', 'active');
-                     msnShoppingGamePane2.resetGame();
-                }
-        
-    }
-    function reloadGame(){
+            console.log("SEL Button received" + selectButton);
+            setTimeout(selectButtonCLick, 1000);
+        } else {
+            console.log("Select Button Null");
+        }
         const msnShoppingGamePane = document.querySelector("shopping-page-base")
-        ?.shadowRoot.querySelector("shopping-homepage")
-        ?.shadowRoot.querySelector("msft-feed-layout")
-        ?.shadowRoot.querySelector("msn-shopping-game-pane");
-        if (msnShoppingGamePane.getAttribute('gamestate')=='win'||msnShoppingGamePane.getAttribute('gamestate')=='idle'||msnShoppingGamePane.getAttribute('gamestate')=='lose') {
+            ?.shadowRoot.querySelector("shopping-homepage")
+            ?.shadowRoot.querySelector("msft-feed-layout")
+            ?.shadowRoot.querySelector("msn-shopping-game-pane");
+        if (msnShoppingGamePane.getAttribute('gamestate') === 'idle') {
             msnShoppingGamePane.setAttribute('gamestate', 'active');
             msnShoppingGamePane.resetGame();
-            console.log("Iteration Number: "+iterationNumber);
+        }
+    }
+
+    async function reloadGame() {
+        const msnShoppingGamePane = document.querySelector("shopping-page-base")
+            ?.shadowRoot.querySelector("shopping-homepage")
+            ?.shadowRoot.querySelector("msft-feed-layout")
+            ?.shadowRoot.querySelector("msn-shopping-game-pane");
+        if (msnShoppingGamePane.getAttribute('gamestate') === 'win' || msnShoppingGamePane.getAttribute('gamestate') === 'idle' || msnShoppingGamePane.getAttribute('gamestate') === 'lose') {
+            msnShoppingGamePane.setAttribute('gamestate', 'active');
+            msnShoppingGamePane.resetGame();
+            console.log("Iteration Number: " + iterationNumber);
             iterationNumber++;
         }
-        
-        if(iterationNumber>=10){
+
+        if (iterationNumber >= 10) {
             clearInterval(fixIntervalId);
         }
     }
-    function selectButtonCLick() {
-        setTimeout(reloadGame, 1000);
+
+    async function selectButtonCLick() {
+        // setTimeout(reloadGame, 1000);
+        await reloadGame();
     }
-    refreshGame();
+
+    await refreshGame();
     msnShoppingGamePane2.addEventListener('click', function (event) {
         console.log("MOUSE");
         refreshGame();
     });
 
     var fixIntervalId;
-    function executeFixFunction() {
+
+    async function executeFixFunction() {
         selectExists = document.querySelector("shopping-page-base")
             ?.shadowRoot.querySelector("shopping-homepage")
             ?.shadowRoot.querySelector("msft-feed-layout")
             ?.shadowRoot.querySelector("msn-shopping-game-pane").getAttribute("gamestate");
         console.log("Execute Fix Function: " + selectExists);
-        if (selectExists == 'win' || selectExists == 'idle' || selectExists == 'active' || selectExists == 'lose') {
+        if (selectExists === 'win' || selectExists === 'idle' || selectExists === 'active' || selectExists === 'lose') {
             refreshGame();
         }
     }
 
-    function startFixExecution() {
+    async function startFixExecution() {
         console.log("Start Fix Function");
-        fixIntervalId = setInterval(function () {
-            executeFixFunction();
-        }, 1000);
+        fixIntervalId = setInterval(async function () {
+            await executeFixFunction();
+        }, 500);
     }
-    startFixExecution();
+
+    await startFixExecution();
 })();
